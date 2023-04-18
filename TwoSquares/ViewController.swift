e58fc3caf3cae208de3f12dac6ed01c287f75510
NetRunner <galatskiy@gmail.com>
2023-04-18 19:24:29 +0300

    Initial Commit

---
+++ TwoSquares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoSquares
+//
+//  Created by Dmitrii Galatskii on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
